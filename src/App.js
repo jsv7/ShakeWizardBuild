665b3fa88cf717f5a9ef657e7d670375e06a4970
jsv7 <jsv7@protonmail.com>
2025-04-10 21:26:38 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { Fragment, useState, useCallback, useEffect } from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { useHapticFeedback } from '@vkruglikov/react-telegram-web-app';
+
+import { viewport, init, isTMA } from "@telegram-apps/sdk";
+
+import { RotatingLines } from "react-loader-spinner";
+
+function Loader() {
+  return (
+    <RotatingLines
+      strokeColor="green"
+      strokeWidth="5"
+      animationDuration="30"
+      width="96"
+      visible={true}
+
+    />
+  )
+}
+
+async function initTg() {
+    if (await isTMA()) {
+        init(); // init tg app
+
+        if (viewport.mount.isAvailable()) {
+            await viewport.mount();
+            viewport.expand(); // first it would be better to expand
+        }
+
+        if (viewport.requestFullscreen.isAvailable()) {
+            await viewport.requestFullscreen(); // then request full screen mode
+        }
+    }
+}
+
+(async () => {
+    await initTg();
+})();
 
 function App() {
+  const [isHapticSoft, setIsHapticSoft] = useState(false);
+  const [isHapticMedium, setIsHapticMedium] = useState(false);
+
+  const { unityProvider, addEventListener, removeEventListener, loadingProgression, isLoaded  } = useUnityContext({
+    loaderUrl: "assets/WEBGL.loader.js",
+    dataUrl: "assets/WEBGL.data.unityweb",
+    frameworkUrl: "assets/WEBGL.framework.js.unityweb",
+    codeUrl: "assets/WEBGL.wasm.unityweb",
+  });
+  const [impactOccurred, notificationOccurred, selectionChanged] =
+    useHapticFeedback();
+
+    function hapticSoft() {
+      notificationOccurred('success');
+    }
+  function hapticMedium() {
+    notificationOccurred('error');
+  }
+
+  const handleHapticSoft = useCallback(() => {
+     hapticSoft();
+   }, []);
+
+   const handleHapticMedium = useCallback(() => {
+      hapticMedium();
+    }, []);
+
+   useEffect(() => {
+     const canvas = document.getElementById("root");
+  if (canvas) {
+    const newWidth = canvas.clientWidth;
+    const newHeigth = canvas.clientHeigth;
+
+    // const dpr = window.devicePixelRatio || 1;
+    // canvas.width = window.innerWidth * dpr;
+    // canvas.height = window.innerHeight * dpr;
+    // canvas.style.width = "100vw";
+    // canvas.style.height = "100vh";
+  }
+
+
+     addEventListener("HapticSoft", handleHapticSoft);
+     addEventListener("HapticMedium", handleHapticMedium);
+
+     return () => {
+       removeEventListener("HapticSoft", handleHapticSoft);
+       removeEventListener("HapticMedium", handleHapticMedium);
+     };
+   }, [addEventListener, removeEventListener, handleHapticSoft]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment >
+     <div className="center">
+          <Loader />
+          {!isLoaded && (
+           <div className="loading-overlay">
+             <div className="loading-spinner"></div>
+             <p>Loading: {Math.round(loadingProgression * 100)}%</p>
+           </div>
+         )}
+     </div>
+
+      <Unity
+      style ={{
+        width: "100vw",   // Full viewport width
+        height: "100vh",  // Full viewport height
+        position: "absolute",
+        top: 0,
+        left: 0,
+        }}
+         unityProvider={unityProvider} />
+    </Fragment>
   );
 }
 
